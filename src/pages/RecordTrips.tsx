6b95973755fe210e00edbba14a5c5a1382767307
Cristian Schuszter <cristian.schuszter@cern.ch>
2020-11-28 18:11:13 +0100

    Make inference work

--- src/pages/RecordTrips.tsx
+++ src/pages/RecordTrips.tsx
@@ -11,7 +11,7 @@ import {
   IonTitle,
   IonToolbar,
 } from "@ionic/react";
-import "./Tab1.css";
+import "./RecordTrips.css";
 import ReactApexChart from "react-apexcharts";
 import instance from "../services/apiCalls";
 import CreateAccount from "../components/CreateAccount";
@@ -22,8 +22,10 @@ import {
   InferenceData,
   InferenceRequest,
   InferenceResponse,
+  TravelType,
 } from "../model/Inference";
 import environment from "../environment";
+import TransportSelectionModal from "./TransportSelectionModal";
 
 const options = {
   chart: {
@@ -52,10 +54,47 @@ const options = {
   labels: ["Your current footprint"],
 };
 
-const Tab1: React.FC = () => {
+const FakeBike = [
+  { lat: 46.25304015892485, lng: 6.0266320669602065, ts: 1606576457052 },
+  { lat: 46.25304015892485, lng: 6.0266320669602065, ts: 1606576457052 },
+  { lat: 46.25304015892485, lng: 6.0266320669602065, ts: 1606576457052 },
+  { lat: 46.25404015892485, lng: 6.0266320669602065, ts: 1606576468945 },
+  { lat: 46.25404015892485, lng: 6.0266320669602065, ts: 1606576468945 },
+  { lat: 46.25414015892485, lng: 6.0266320669602065, ts: 1606576482177 },
+  { lat: 46.25414015892485, lng: 6.0266320669602065, ts: 1606576482177 },
+  { lat: 46.25424015892485, lng: 6.0266320669602065, ts: 1606576490761 },
+  { lat: 46.25424015892485, lng: 6.0266320669602065, ts: 1606576490761 },
+  { lat: 46.25434015892485, lng: 6.0266320669602065, ts: 1606576500593 },
+  { lat: 46.25434015892485, lng: 6.0266320669602065, ts: 1606576500593 },
+  { lat: 46.25434015892485, lng: 6.0266320669602065, ts: 1606576504630 },
+];
+
+const FakeWalk = [
+  { lat: 46.25434015892485, lng: 6.0266320669602065, ts: 1606576504630 },
+  { lat: 46.25434015892485, lng: 6.0266320669602065, ts: 1606576504630 },
+  { lat: 46.25434015892485, lng: 6.0266320669602065, ts: 1606576504630 },
+  { lat: 46.25444015892485, lng: 6.0266320669602065, ts: 1606576835730 },
+  { lat: 46.25444015892485, lng: 6.0266320669602065, ts: 1606576835730 },
+  { lat: 46.25544015892485, lng: 6.0266320669602065, ts: 1606576841673 },
+  { lat: 46.25544015892485, lng: 6.0266320669602065, ts: 1606576841673 },
+  { lat: 46.25644015892485, lng: 6.0266320669602065, ts: 1606576859401 },
+  { lat: 46.25644015892485, lng: 6.0266320669602065, ts: 1606576859401 },
+  { lat: 46.25664015892485, lng: 6.0266320669602065, ts: 1606576868945 },
+  { lat: 46.25664015892485, lng: 6.0266320669602065, ts: 1606576868945 },
+  { lat: 46.25684015892485, lng: 6.0266320669602065, ts: 1606576876880 },
+  { lat: 46.25684015892485, lng: 6.0266320669602065, ts: 1606576876880 },
+  { lat: 46.25684015892485, lng: 6.0266320669602065, ts: 1606576879889 },
+];
+
+const RecordTrips: React.FC = () => {
   const [footprint, setFootprint] = useState(0);
   const [isTravelling, setIsTravelling] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+  const [show, setShowModal] = useState(false);
+
+  const [inference, setInferenceResponse] = useState<InferenceResponse | null>(
+    null
+  );
 
   const fetchURL = "";
 
@@ -96,26 +135,45 @@ const Tab1: React.FC = () => {
       ts: d.timestamp,
     }));
 
+    // console.log(FakeBike);
+    // console.log(reqData);
+
     const inferenceResponse = await Axios.post<
       InferenceRequest,
       AxiosResponse<InferenceResponse>
-    >(environment.inferenceEndpoint, { points: reqData });
-    console.log(inferenceResponse.data);
+    >(environment.inferenceEndpoint, { points: FakeBike });
+
+    setInferenceResponse(inferenceResponse.data);
+    setShowModal(true);
     setIsLoading(false);
+  };
+
+  const submitTrip = async (data: InferenceResponse) => {
+    setIsLoading(true);
+    await instance.post("/api/footprint/trip", data);
+    setIsLoading(false);
+    setShowModal(false);
   };
 
   return (
     <IonPage>
+      {inference && (
+        <TransportSelectionModal
+          show={show}
+          resp={inference}
+          onSubmit={(d) => submitTrip(d)}
+        />
+      )}
       <IonLoading isOpen={isLoading} message={"Inferring..."} />
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>Record trip</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
+            <IonTitle size="large">Record trip</IonTitle>
           </IonToolbar>
         </IonHeader>
         <IonGrid>
@@ -140,4 +198,4 @@ const Tab1: React.FC = () => {
   );
 };
 
-export default Tab1;
+export default RecordTrips;
---
+++ src/pages/TransportSelectionModal.tsx
@@ -0,0 +1,154 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import {
+  IonAvatar,
+  IonButton,
+  IonContent,
+  IonGrid,
+  IonHeader,
+  IonIcon,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonListHeader,
+  IonModal,
+  IonRow,
+  IonSelect,
+  IonSelectOption,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import { InferenceResponse, TravelType } from "../model/Inference";
+
+import { walk, walkSharp, bicycle, car, leaf, send } from "ionicons/icons";
+
+import { transport } from "carbon-footprint";
+
+export interface TransportSelectionModalProps {
+  show: boolean;
+  resp: InferenceResponse;
+  onSubmit: (d: InferenceResponse) => void;
+}
+
+const resultToIcon = (transport: TravelType) => {
+  switch (transport) {
+    case TravelType.biking:
+      return bicycle;
+    case TravelType.walking:
+      return walk;
+    case TravelType.jogging:
+      return walkSharp;
+    case TravelType.driving:
+      return car;
+    default:
+      return car;
+  }
+};
+
+const resultToCarbonValue = (tr: TravelType) => {
+  switch (tr) {
+    case TravelType.biking:
+      return transport.motorbike;
+    case TravelType.walking:
+      return 0;
+    case TravelType.jogging:
+      return 0;
+    case TravelType.driving:
+      console.log(transport.car);
+      return transport.car;
+    default:
+      return transport.car;
+  }
+};
+
+export const TransportSelectionModal: React.FC<TransportSelectionModalProps> = ({
+  show,
+  resp,
+  onSubmit,
+}) => {
+  const [formData, setFormData] = useState(resp);
+
+  console.log(formData);
+
+  const producedCO2 =
+    formData.totalDistance * resultToCarbonValue(formData.transport);
+
+  return (
+    <IonModal isOpen={show} cssClass="my-custom-class">
+      <IonContent fullscreen class="ion-padding">
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>Inference details - confirm </IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonList>
+          <p>We have detected the following details about your journey:</p>
+          <IonItem>
+            <IonAvatar>
+              <IonAvatar>
+                <IonIcon
+                  size="large"
+                  icon={resultToIcon(formData.transport)}
+                  color={"white"}
+                />
+              </IonAvatar>
+            </IonAvatar>
+            <IonLabel>
+              Traveled {formData.totalDistance.toFixed(2)} km by{" "}
+              {formData.transport}
+            </IonLabel>
+          </IonItem>
+          <IonItem>
+            <IonAvatar>
+              <IonAvatar>
+                <IonIcon size="large" icon={leaf} color={"green"} />
+              </IonAvatar>
+            </IonAvatar>
+            <IonLabel>
+              <h3>Recording this trip adds</h3>
+              {producedCO2 !== 0 && <p> {producedCO2.toFixed(6)} kgCO2</p>}
+              {!producedCO2 && (
+                <p style={{ color: "green" }}>0 kgCO2, congrats!</p>
+              )}
+              <p>Please click below to change transport.</p>
+            </IonLabel>
+          </IonItem>
+          <IonItem>
+            <IonAvatar>
+              <IonAvatar>
+                <IonIcon size="large" icon={send} color={"white"} />
+              </IonAvatar>
+            </IonAvatar>
+            <IonLabel>
+              <h3>Your average velocity:</h3>
+              <p> {formData.avgSpeed.toFixed(1)} km/h.</p>
+            </IonLabel>
+          </IonItem>
+          <IonItem>
+            <IonLabel>Transport mode</IonLabel>
+            <IonSelect
+              value={formData.transport}
+              okText="Okay"
+              cancelText="Dismiss"
+              onIonChange={(e) =>
+                setFormData({ ...formData, transport: e.detail.value })
+              }
+            >
+              <IonSelectOption value="walking">Walking</IonSelectOption>
+              <IonSelectOption value="jogging">Jogging</IonSelectOption>
+              <IonSelectOption value="biking">Biking</IonSelectOption>
+              <IonSelectOption value="driving">Driving</IonSelectOption>
+            </IonSelect>
+          </IonItem>
+        </IonList>
+      </IonContent>
+      <IonButton expand="block" onClick={() => onSubmit(formData)}>
+        Submit
+      </IonButton>
+    </IonModal>
+  );
+};
+
+TransportSelectionModal.propTypes = {};
+
+export default TransportSelectionModal;
